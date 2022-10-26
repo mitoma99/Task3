9b0bf388ef2ce9f1da29d7361f12e84d7d7a3a46
mitoma99 <114859605+mitoma99@users.noreply.github.com>
2022-10-26 07:36:56 +0000

    [Update6]task3

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :is_matching_login_user, only: [:edit, :update]
+  
   def index
     @book = Book.new
     @users = User.all
@@ -6,19 +8,23 @@ class UsersController < ApplicationController
   end
 
   def show
-    @book = Book.new
     @user = User.find(params[:id])
+    @book = Book.new
     @books = @user.books
   end
 
   def edit
-    @user = User.find(params[:id])
+      @user = User.find(params[:id])
   end
 
   def update
     @user = User.find(params[:id])
-    @user.update(user_params)
-    redirect_to user_path(@user.id)
+    if @user.update(user_params)
+    flash[:notice]="You have updated user successfully."
+      redirect_to user_path(@user.id)
+    else
+      render :edit
+    end
   end
 
   private
@@ -26,5 +32,15 @@ class UsersController < ApplicationController
   def user_params
     params.require(:user).permit(:name, :profile_image, :introduction)
   end
+  
+  def is_matching_login_user
+    user_id = params[:id].to_i
+    login_user_id = current_user.id
+    if(user_id != login_user_id)
+      redirect_to user_path(current_user.id)
+    end 
+  end 
 
 end
+
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
 
-  get '' => 'homes#top'
+  #get '' => 'homes#top'
+  root to: 'homes#top'
 
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
@@ -8,6 +9,6 @@ Rails.application.routes.draw do
  resources :books, only: [:new,:create,:index,:show,:edit,:update,:destroy]
  resources :users, only: [:index,:show,:edit,:update]
 
- get 'homes/about' => 'homes#about',as:'about'
+ get 'home/about' => 'homes#about'
 
 end
